8bcec4bb461868b3dea0afccbc48b35565e27a86
Denis Calin <deniscalin.pro@gmail.com>
2024-10-05 10:37:24 -0700

    Implement training loop and val loss evals

--- digit_mlp.py
+++ digit_mlp.py
@@ -114,3 +114,142 @@ class Tanh:
     
     def parameters(self):
         return []
+    
+
+# INITIALIZE THE NETWORK
+
+# Removing embeddings
+# n_embed = 10 # the dimensionality of the character embedding vectors
+n_hidden = 512 # the number of neurons in each layer
+vocab_size = 10
+
+g = torch.Generator().manual_seed(42) # Generator for reproducibility
+
+# Removing embeddings
+# Setting the input to be 784
+# C = torch.randn((vocab_size, n_embed), generator=g)
+
+# 12 Layers
+layers = [Linear(784, n_hidden),                  BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, n_hidden),             BatchNorm1d(n_hidden), Tanh(),
+          Linear(n_hidden, vocab_size),           BatchNorm1d(vocab_size)
+         ]
+
+# Kaiming init
+with torch.no_grad():
+    #last layer: make less confident
+    # layers[-1].weight *= 0.1
+    layers[-1].gamma *= 0.1
+    
+    # for all other layers: multiply by gain for tanh (5/3)
+    for layer in layers[:-1]:
+        if isinstance(layer, Linear):
+            layer.weight *= 5/3
+            
+parameters = [p for layer in layers for p in layer.parameters()]
+print(sum(p.nelement() for p in parameters))
+
+for p in parameters:
+    p.requires_grad = True
+
+
+# TRAINING LOOP
+
+n_steps = 140000
+batch_size = 32
+lossi = []
+ud = []
+
+for i in range(n_steps):
+    # Contruct the minibatch
+    ix = torch.randint(0, x_train.shape[0], (batch_size,), generator=g)
+    Xb, yb = x_train[ix], y_train[ix] # batch X and y
+    # Normalize the input
+    Xb = (Xb - torch.min(Xb)) / (torch.max(Xb) - torch.min(Xb))
+     
+    # Forward pass 
+    # emb = C[Xb]
+    x = Xb.view(ix.shape[0], -1)
+    yb = yb.squeeze(1)
+    for layer in layers:
+        x = layer(x)
+    loss = F.cross_entropy(x, yb)
+    
+    # Backward pass
+    for layer in layers:
+        layer.out.retain_grad()
+    for p in parameters:
+        p.grad = None
+    loss.backward()
+    
+    # LR SCHEDULE, UPDATING THE PARAMS
+    if i < 50000:
+        lr = 1e-1
+    elif i > 50000 and i < 70000:
+        lr = 1e-2
+    elif i > 70000 and i < 100000:
+        lr = 1e-3
+    elif i > 100000:
+        lr = 3e-4
+    # lr = 1e-1 if i < 40000 else 1e-3
+    for p in parameters:
+        p.data += -lr * p.grad
+        
+    # Track stats
+    if i % 500 == 0: # print every 10,000 steps
+        print(f'lr: {lr:.4f} | {i:7d}/{n_steps:7d}: {loss.item():.4f}')
+    lossi.append(loss.item())
+    with torch.no_grad():
+        ud.append([(lr * p.grad.std() / p.data.std()).log10().item() for p in parameters])
+
+        
+# Put all batchnorm layers in inference mode
+for layer in layers:
+    if isinstance(layer, BatchNorm1d):
+        print('Setting the BatchNorm1d layer in inference mode')
+        layer.training = False
+
+
+# VALIDATION LOSS EVALS
+out = []
+accs = []
+# batch_size = 32
+runs = 40000
+print(f"Starting the eval with {runs} runs on validation data")
+
+for i in range(runs):
+    # Contruct the minibatch
+    ix = torch.randint(0, x_val.shape[0], (batch_size,))
+    X_valb, y_valb = x_val[ix], y_val[ix] # batch X and y
+    # Normalize the input
+    X_valb = (X_valb - torch.min(X_valb)) / (torch.max(X_valb) - torch.min(X_valb))
+
+    # Forward pass 
+    # emb = C[Xb]
+    x = X_valb.view(ix.shape[0], -1)
+    # y_valb = y_valb.squeeze(1)
+    for layer in layers:
+        x = layer(x)
+    # Add some scaling?
+    # x = x / x.shape[1]
+    probs = F.softmax(x, dim=0)
+    ix = torch.multinomial(probs, num_samples=1, replacement=True, generator=g)
+    out.append(ix)
+    # print(out)
+    # print(y_valb)
+    acc = torch.sum(ix == y_valb) / batch_size
+    # print(f"Accuracy: {acc}")
+    accs.append(acc)
+    
+mean_accuracy = sum(accs) / len(accs)
+print(f"Training run config: {n_steps} steps, {lr} final lr")
+print(f"Mean accuracy over {runs} runs: {mean_accuracy}")
