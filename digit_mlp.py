491938e8ccaf6b684cfb77ff4beb16edad26cd8b
Denis Calin <deniscalin.pro@gmail.com>
2024-10-05 10:34:28 -0700

    Implement data pipeline, model classes, model init

--- digit_mlp.py
+++ digit_mlp.py
@@ -1,64 +1,116 @@
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import json
+import math
+# from dataclasses import dataclass, field
+import torch
+import torch.nn.functional as F
+import random
+import matplotlib.pyplot as plt
 
 
-# @dataclass
-# class MLPConfig:
-#     size: list = field(default_factory=list)
-#     epochs: int = 10
-#     lr: float = 1e-3
+# READ IN THE DATA
+train_data = pd.read_csv('emnist-digits-train.csv')
+val_data = pd.read_csv('emnist-digits-test.csv')
+
+# PRINT PD DATA SHAPE
+print(f"Train data pd shape: {train_data.shape}, val data pd shape: {val_data.shape}")
+
+# CONVERT DATA TO NUMPY ARRAYS AND SHUFFLE
+random.seed(42)
+numpy_train_data = np.array(train_data)
+numpy_val_data = np.array(val_data)
+print("Train data before shuffling: ", numpy_train_data[:, 0])
+print("Val data before shuffling: ", numpy_val_data[:, 0])
+random.shuffle(numpy_train_data)
+random.shuffle(numpy_val_data)
+print("Train data after shuffling: ", numpy_train_data[:, 0])
+print("Val data after shuffling: ", numpy_val_data[:, 0])
+
+# CREATE X AND Y SPLITS FOR BOTH SETS
+print("Creating x and y splits for train and val sets")
+x_train = numpy_train_data[:, 1:]
+y_train = numpy_train_data[:, :1]
+
+x_val = numpy_val_data[:, 1:]
+y_val = numpy_val_data[:, :1]
+
+x_train = torch.tensor(x_train, dtype=torch.float)
+y_train = torch.tensor(y_train)
+
+x_val = torch.tensor(x_val, dtype=torch.float)
+y_val = torch.tensor(y_val)
+
+print("x_train.shape, y_train.shape, x_val.shape, y_val.shape: ", 
+      x_train.shape, y_train.shape, x_val.shape, y_val.shape,)
+
+# TRANSPOSE EACH ROW/IMAGE
+print("Transposing each row/image in both train and val sets")
+for i in range(x_train.shape[0]):
+    x_train[i] = x_train[i].reshape((28, 28)).T.reshape((784))
+
+for i in range(x_val.shape[0]):
+    x_val[i] = x_val[i].reshape((28, 28)).T.reshape((784))
+
+# MLP MODEL
+# Classes of the model
 
 
-class MLP:
+class Linear:
     
-    def __init__(self, sizes, epochs, lr):
-        self.sizes = sizes
-        self.epochs = epochs
-        self.lr = lr
+    def __init__(self, fan_in, fan_out, bias=True):
+        self.weight = torch.randn((fan_in, fan_out), generator=g) / (fan_in**0.5)
+        self.bias = torch.zeros((fan_out)) if bias else None
         
-        input_layer = sizes[0]
-        hidden_layer_1 = sizes[1]
-        hidden_layer_2 = sizes[2]
-        output_layer = sizes[3]
-        
-        self.params = {
-            "W1": np.random.randn(input_layer, hidden_layer_1),      # 768, 128
-            "W2": np.random.randn(hidden_layer_1, hidden_layer_2),   # 128, 64
-            "W3": np.random.randn(hidden_layer_2, output_layer)      # 64, 10
-        }
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
     
-    def forward(self, x):
-        
-        w1_out = np.dot()
-        
+    def parameters(self):
+        return [self.weight] + ([] if self.bias is None else [self.bias])
     
-    def backward(self):
-        pass
+
+class BatchNorm1d:
     
-    def evaluate(self):
-        pass
-
-
-# Get the data
-data = pd.read_csv('train.csv')
-
-# Convert to a numpy array
-numpy_data = np.array(data)
-
-# Shuffle in place
-np.random.shuffle(numpy_data)
-
-# Separate into train and val sets
-n = int(len(numpy_data) * 0.8)
-train_data = numpy_data[:n]
-val_data = numpy_data[n:]
-
-# Create x and y splits for each set
-x_train = train_data[:, 1:]
-y_train = train_data[:, :1]
-
-x_val = val_data[:, 1:]
-y_val = val_data[:, :1]
-
-#
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+        # Trained parameters
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        # Buffers (trained with momentum update)
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+        
+    def __call__(self, x):
+        # Calculate the forward pass
+        if self.training:
+            xmean = x.mean(0, keepdim=True) # batch mean
+            xvar = x.var(0, keepdim=True, unbiased=True) # batch variance
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+        # Update the buffers
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+        return self.out
+    
+    def parameters(self):
+        return [self.gamma, self.beta]
+    
+    
+class Tanh:
+    
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+    
+    def parameters(self):
+        return []
